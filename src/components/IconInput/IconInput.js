f28c77c7018cf441153d8405ca90bc0ae7b96b15
gianlazaro <hello@gianlazaro.com>
2022-01-15 18:23:12 -0800

    Initial commit

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const styles = {
+    small: {
+      "border-bottom": "1px solid black",
+    },
+    large: {
+      "border-bottom": "3px solid black",
+    },
+  };
+  return (
+    <Wrapper style={{ "--width": width + "px", ...styles[size] }}>
+      <SearchBar placeholder={placeholder} />
+    </Wrapper>
+  );
 };
 
+const SearchBar = styled.input`
+  width: 100%;
+  border: 0;
+  line-height: 1.5;
+  font-weight: 700;
+  color: ${COLORS.gray700};
+  &:focus {
+    outline-offset: 4px;
+  }
+  &::placeholder {
+    font-weight: 300;
+  }
+  &:hover,
+  &:active {
+    color: ${COLORS.black};
+  }
+`;
+
+const Wrapper = styled.form`
+  width: var(--width);
+  background: red;
+`;
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,67 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let Component;
+  if (size === "small") {
+    Component = SmallBar;
+  } else if (size === "medium") {
+    Component = MediumBar;
+  } else {
+    Component = LargeBar;
+  }
+  return (
+    <Component
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      size={size}
+    >
+      <BarFillWrapper>
+        <BarFill style={{ "--percentage": value + "%" }} value={value}>
+          <VisuallyHidden>{value}</VisuallyHidden>
+        </BarFill>
+      </BarFillWrapper>
+    </Component>
+  );
 };
+
+const BarWrapper = styled.div`
+  width: 370px;
+  background: ${COLORS.transparentGray15};
+  border-radius: 4px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const SmallBar = styled(BarWrapper)`
+  height: 8px;
+`;
+const MediumBar = styled(BarWrapper)`
+  height: 12px;
+`;
+const LargeBar = styled(BarWrapper)`
+  height: 24px;
+  padding: 4px;
+  border-radius: 8px;
+`;
+
+const BarFill = styled.div`
+  background: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+  width: var(--percentage);
+  height: 100%;
+`;
+
+const BarFillWrapper = styled.div`
+  height: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+  /* background: red; */
+`;
 
 export default ProgressBar;
